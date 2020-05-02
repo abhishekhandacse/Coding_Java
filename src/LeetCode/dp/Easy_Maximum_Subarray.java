b00b1546c8ef09166daee73aaf6347662c0c0efe
Abhishek Handa <abhishekhandacse@gmail.com>
2020-05-02 16:54:54 +0530

    Maximum subarray done with 4 different approaches

--- src/LeetCode/dp/Easy_Maximum_Subarray.java
+++ src/LeetCode/dp/Easy_Maximum_Subarray.java
@@ -10,7 +10,7 @@ package LeetCode.dp;
 public class Easy_Maximum_Subarray{
 /*
 
-    // Approach 1   TC=O(N^3)
+    // Approach 1   TC=O(N^3)       SC=O(N)
     public int maxSubArray(final int[] nums) {
               if(nums==null || nums.length==0)return 0;
               int max=Integer.MIN_VALUE;
@@ -31,26 +31,96 @@ public class Easy_Maximum_Subarray{
 
 */
 
+/*
 
-    // Approach 2   TC=O(N^2)
+
+    // Approach 2   TC=O(N^2)       SC=O(N)
     public int maxSubArray(final int[] nums) {
         if(nums==null || nums.length==0)return 0;// Invalid
         int max=Integer.MIN_VALUE;
-        int sum=0;
         int[] prefix=new int[nums.length+1];
         // Generate the prefix sum
         for(int i=0;i<nums.length;i++){
+            prefix[i+1]=prefix[i] + nums[i];
+        }
+
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
             for(int j=i;j< nums.length;j++){
-                //Compute sum b/w i-->j
-                sum=0;
-                for(int k=i;k<=j;k++){
-                    sum+=nums[k];
-                }
+                sum=prefix[j+1] - prefix[i];
                 if(max<sum)
                     max=sum;
             }
         }
-        return max;
+          return max;
+    }
+ */
+/*
+
+        // Approach 3   TC=O(NlogN)       SC=O(N)           // Quite Similer to merge sort
+        private int Conquer(final int[] arr,int low,int mid,int high){
+
+                    int left_contagious_sum=Integer.MIN_VALUE;
+                    int right_contagious_sum=Integer.MIN_VALUE;
+
+                    // Moving to left side from mid included to low included
+                    int temp_left_sum=0;
+                    for(int i=mid;i>=low;i--){
+                        temp_left_sum+=arr[i];
+                        if(left_contagious_sum<temp_left_sum)
+                            left_contagious_sum=temp_left_sum;
+                    }
+
+                    // Moving to right side from mid excluded to high included
+                    int temp_right_sum=0;
+                    for(int i=mid+1;i<=high;i++){
+                        temp_right_sum+=arr[i];
+                        if(right_contagious_sum<temp_right_sum)
+                            right_contagious_sum=temp_right_sum;
+                    }
+
+                    return left_contagious_sum+right_contagious_sum;
+        }
+        private int Divide(final int[] arr,int low,int high){
+                    if(low==high)return arr[low];      // Single element is itself the maximum sum in that subarray
+
+                    int mid=(low + (high-low)/2);
+
+                    int left_sum=  Divide(arr,low,mid);        // Divide the array with left part containing extra element in case of total odd elements
+                    int right_sum= Divide(arr,mid+1,high);
+                    // Conquer step or merge step
+                    int merged_sum=Conquer(arr,low,mid,high);// This step would provide their maximum sum considering them contagious
+                    return Math.max(left_sum,Math.max(right_sum,merged_sum));
+        }
+        public int maxSubArray(final int[] nums) {
+            if (nums == null || nums.length == 0) return 0;// Invalid
+            return Divide(nums,0,nums.length-1);
+        }
+
+
+        */
+
+
+        // Approach 4    kadanes Algorithm        TC(O(N))   SC(O(N))   // Basically sliding window approach
+                                                                        // The Main idea is that, we should restart counting the sum, as soon as sum becomes -ve
+
+
+
+    public int maxSubArray(final int[] nums) {
+        if (nums == null || nums.length == 0) return 0;// Invalid
+        int maxsum=Integer.MIN_VALUE;
+        int currsum=0;
+
+        for(int i=0;i<nums.length;i++){
+            currsum+=nums[i];
+            if(maxsum<currsum)
+                maxsum=currsum;
+
+            if(currsum<0)
+                currsum=0;//Rest for fresh start
+
+        }
+        return maxsum;
     }
 
 
@@ -62,7 +132,7 @@ public class Easy_Maximum_Subarray{
         Easy_Maximum_Subarray obj=new Easy_Maximum_Subarray();
         int[] arr=new int[]{-2,1,-3,4,-1,2,1,-5,4};//6
         System.out.println(obj.maxSubArray(arr));
-        arr=new int[]{-10,-20,-30,-40};//0
+        arr=new int[]{-10,-20,-30,-40};//-10
         System.out.println(obj.maxSubArray(arr));
         arr=new int[]{-10,20,-30,40};//40
         System.out.println(obj.maxSubArray(arr));
