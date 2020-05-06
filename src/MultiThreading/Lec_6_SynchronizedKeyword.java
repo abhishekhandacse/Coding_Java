4ff816628dc0f50667c2dad4d5bd28fed03a6269
Abhishek Handa <abhishekhandacse@gmail.com>
2020-05-07 02:37:10 +0530

    Practices more multi-threading conceps for IPC like wait() and notify()

---
+++ src/MultiThreading/Lec_6_SynchronizedKeyword.java
@@ -0,0 +1,39 @@
+package MultiThreading;
+
+class Counter{
+    int count;
+    Counter(){count=0;}
+
+    public synchronized void  increment(){
+        count=count+1;
+    }
+}
+
+public class Lec_6_SynchronizedKeyword {
+    public static void main(String[] args) throws InterruptedException {
+        Counter c=new Counter();
+        System.out.println("Count is " + c.count);
+
+        Thread t1=new Thread(
+                ()->{
+            for(int i=1;i<=1000;i++){
+                c.increment();
+            }
+        }
+        );
+
+        Thread t2=new Thread(
+                ()->{
+                    for(int i=1;i<=1000;i++){
+                        c.increment();
+                    }
+                }
+        );
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("Count " + c.count);
+        // The synchronized keyword ensures that only single thread enters inside increment function and other waits for the method to become free.
+    }
+}
---
+++ src/MultiThreading/Lec_7_Producer_Consumer_Problem.java
@@ -0,0 +1,86 @@
+package MultiThreading;
+
+class Q{
+    int num;
+    boolean valueSet=false;
+    public synchronized void put(int num) {
+        while(valueSet){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("PUT " + num);
+        this.num=num;
+        valueSet=true;
+        notify();
+    }
+
+    public synchronized void get(){
+        while (!valueSet){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("GET " + num);
+        valueSet=false;
+        notify();
+    }
+}
+
+class Producer implements Runnable{
+    Q q;
+    public Producer(Q q){
+        this.q=q;
+        Thread t=new Thread(this,"Producer");
+        t.start();
+    }
+    public void run(){
+        int i=0;
+        while(true){
+            q.put(i++);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
+
+class Consumer implements Runnable{
+    Q q;
+    public Consumer(Q q){
+        this.q=q;
+        Thread t=new Thread(this,"Consumer");
+        t.start();
+    }
+    public void run(){
+
+        while(true){
+            q.get();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
+
+
+public class Lec_7_Producer_Consumer_Problem {
+
+    public static void main(String[] args) {
+        Q q=new Q();
+
+        new Producer(q);
+        new Consumer(q);
+    }
+
+}
