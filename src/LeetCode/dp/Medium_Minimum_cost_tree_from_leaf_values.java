a79228f364d0d2e22f7a402578dcac63b3c02a7f
Abhishek Handa <abhishekhandacse@gmail.com>
2020-05-12 21:42:58 +0530

    Submitted Medium Question Next Greater Element 2

---
+++ src/LeetCode/dp/Medium_Minimum_cost_tree_from_leaf_values.java
@@ -0,0 +1,157 @@
+package LeetCode.dp;
+
+public class Medium_Minimum_cost_tree_from_leaf_values {
+            // Time Complexity = O(Len ^ 3)         Space Complexity = O(Len ^ 2)
+            static final boolean DEBUG=false;
+    public int mctFromLeafValues(int[] arr) {
+                if(arr==null || arr.length==0)return Integer.MIN_VALUE; // This is not possible as mentioned in constraints
+                int[][] Maximum_Matrix=new int[arr.length][arr.length];// This will store the maximum value in array range from i-->j (both included)
+                int[][] DP=new int[arr.length][arr.length];// This will store the answer to sub-problem (Remember if we are trying to partition the arr with a single leaf node on one side then its contribution would be 0)
+                //Populating the Maximum_Matrix from arr
+                for(int i=0;i<arr.length;i++){
+                    int max=arr[i];
+                    for(int j=i;j<arr.length;j++){
+                        if(max<arr[j])
+                            max=arr[j];
+                        Maximum_Matrix[i][j]=max;
+                    }
+                }
+                if(DEBUG){
+                    for(int i=0;i<arr.length;i++){
+                        for(int j=0;j<arr.length;j++){
+                            System.out.print(Maximum_Matrix[i][j]+ " ");
+                        }
+                        System.out.println();
+                    }
+                }
+                // Populating the DP matrix from smaller sub-problems already evaluated and stored in dp arr
+                // I am populating the DP matrix from principal diagonal towards the right top edge of the matrix
+                // The principal value of DP matrix is 0 because I can't make internal node out of it
+                int i,j,Row=0,Col=1;    // Minimum 2 elements are present in the arr is ensured
+                int iterations=(arr.length-1);  // populating all the diagonals just after principal diagonal
+
+                while(iterations!=0){
+                    i=Row;j=Col;    // Starting diagnol type traversal from Row , Col
+                    while ( (i!=arr.length) && (j!=arr.length) ){
+                        int min_sum=Integer.MAX_VALUE;  // This is minimum possible sum of internal nodes
+                        for(int partition=i;partition<j;partition++){   // This loop considers all partitions possible without making one side as null
+                            // This is computing the so formed root node using maximum from left and right sub-trees respective maximum
+                            // I already know the internal nodes in left subtree is DP[i][partition] and right subtree is DP[partition+1][j]
+                            // justing adding the root node to left sub tree internal sum and right sub tree internal sum would give me sum for this sub-problem
+                            int curr_sum=Maximum_Matrix[i][partition]*Maximum_Matrix[partition+1][j] + DP[i][partition] + DP[partition+1][j];
+                            if(min_sum>curr_sum)
+                                min_sum=curr_sum;
+                        }
+                        DP[i][j]=min_sum;
+
+                        i++;
+                        j++;
+                    }
+                    Col++;
+                    iterations--;
+                }
+
+
+                return DP[0][arr.length-1];
+    }
+
+
+
+
+
+    /*
+    * Greedy solution inspiration is taken from https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/discuss/339959/One-Pass-O(N)-Time-and-Space
+    * Franky speaking, I was unable to trace the greedy algorithm even after spending entire day of brain-storming.
+    * I also believe before attempting to solve a question with dp algorithm, we need to thoroughly ensure ourselves that greedy solution is not possible. If greedy solution is possible than generally speaking it would be more efficient than dp solution in terms of both space and time complexity.
+    */
+
+
+
+
+    // TC is O(N^2) due to skipping deleted value in arrays and SC is O(1)
+    // The innermost while loops will run atmost 40 times in the worst case and could be ignored // Or You can use linkedlist to save even those computations
+    public int mctFromLeafValues_Optimized(int[] arr) {
+        if(arr==null || arr.length==0)return Integer.MAX_VALUE; // This is not possible as mentioned in constraints
+        if(arr.length==1)return Integer.MAX_VALUE;
+
+        int num_of_elements=arr.length;
+
+        int cost=0;
+        while(num_of_elements>1){
+            int i=0,j=0;
+            int min_index=0;
+            int min_prod=Integer.MAX_VALUE;
+            for(int k=0;k<num_of_elements-1;k++){
+                if(i==j){
+                    j++;
+                }
+                while(arr[i]==Integer.MAX_VALUE)
+                    i++;
+                while(arr[j]==Integer.MAX_VALUE || (i==j))
+                    j++;
+                if(min_prod>(arr[i]*arr[j]) ){
+                    min_prod=(arr[i]*arr[j]);
+                    min_index=((arr[i]<arr[j])?i:j);
+                }
+                i=i+1;
+                j=i+1;
+            }
+            cost+=min_prod;
+            arr[min_index]=Integer.MAX_VALUE;
+            num_of_elements--;
+        }
+        return cost;
+
+    }
+
+    /*
+    *   The above approach could be optimized using LinkedList to store all the elements
+    *   Now we can save the precious while loop skipping the deleted values since we are directly deleting the element :)
+    *   Time Complexity -: O(N^2) Space Complexity -: O(N)
+    */
+
+    public int mctFromLeafValues_LinkedList_Optimized(int[] arr) {
+        if(arr==null || arr.length==0)return Integer.MAX_VALUE; // This is not possible as mentioned in constraints
+        if(arr.length==1)return Integer.MAX_VALUE;
+
+        int num_of_elements=arr.length;
+
+        int cost=0;
+        while(num_of_elements>1){
+            int i=0,j=0;
+            int min_index=0;
+            int min_prod=Integer.MAX_VALUE;
+            for(int k=0;k<num_of_elements-1;k++){
+                if(i==j){
+                    j++;
+                }
+                while(arr[i]==Integer.MAX_VALUE)
+                    i++;
+                while(arr[j]==Integer.MAX_VALUE || (i==j))
+                    j++;
+                if(min_prod>(arr[i]*arr[j]) ){
+                    min_prod=(arr[i]*arr[j]);
+                    min_index=((arr[i]<arr[j])?i:j);
+                }
+                i=i+1;
+                j=i+1;
+            }
+            cost+=min_prod;
+            arr[min_index]=Integer.MAX_VALUE;
+            num_of_elements--;
+        }
+        return cost;
+
+    }
+
+    // Unit testing
+    public static void main(String[] args) {
+        Medium_Minimum_cost_tree_from_leaf_values obj=new Medium_Minimum_cost_tree_from_leaf_values();
+        int[] arr=new int[]{6,2,4};
+        System.out.println(obj.mctFromLeafValues_Optimized(arr));//32
+        arr=new int[]{6,2,4,8};
+        System.out.println(obj.mctFromLeafValues_Optimized(arr));//80
+        arr=new int[]{15,13,5,3,15};
+        System.out.println(obj.mctFromLeafValues_Optimized(arr));//500
+    }
+}
---
+++ src/LeetCode/stacks/Medium_NextGreaterElement2.java
@@ -0,0 +1,74 @@
+package LeetCode.stacks;
+
+import java.util.Stack;
+
+class node{
+    int element;
+    int index;
+    node(int element,int index){
+        this.element=element;
+        this.index=index;
+    }
+}
+// Time Complexity O(N)     Space Complexity O(N)
+public class Medium_NextGreaterElement2 {
+    public int[] nextGreaterElements(int[] arr) {
+        int len=arr.length;
+        if(arr==null)return null;
+        if(arr.length==0)return new int[0];
+        int ans[]=new int[len];
+        for(int i=0;i<len;i++)ans[i]=-1; // Initializing ans with -1
+
+        Stack<node> st=new Stack<>();
+        st.push(new node(arr[0],0));
+
+        for(int i=1;i<((2*len)-1);i++){
+
+            while(!st.isEmpty() && (arr[i%len]>st.peek().element)){
+                ans[st.peek().index]=arr[(i%len)];
+                st.pop();
+            }
+            st.push(new node(arr[(i%len)],(i%len)));
+        }
+        return ans;
+    }
+//  For Unit testing
+    public static void main(String[] args) {
+        Medium_NextGreaterElement2 obj=new Medium_NextGreaterElement2();
+
+        int arr[]=obj.nextGreaterElements(new int[]{1,2,1});
+        for(int i:arr)
+            System.out.print(i+" ");
+        System.out.println();
+
+        arr=obj.nextGreaterElements(new int[]{2,2,2});
+        for(int i:arr)
+            System.out.print(i+" ");
+        System.out.println();
+
+        arr=obj.nextGreaterElements(new int[]{10,15,0,20});
+        for(int i:arr)
+            System.out.print(i+" ");
+        System.out.println();
+
+        arr=obj.nextGreaterElements(new int[]{15,12,10,8});
+        for(int i:arr)
+            System.out.print(i+" ");
+        System.out.println();
+
+        arr=obj.nextGreaterElements(new int[]{1,2,3,4});
+        for(int i:arr)
+            System.out.print(i+" ");
+        System.out.println();
+
+        arr=obj.nextGreaterElements(new int[]{1,2,3,2,1});
+        for(int i:arr)
+            System.out.print(i+" ");
+        System.out.println();
+
+
+    }
+
+}
+
+
